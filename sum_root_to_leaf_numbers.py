e94378b5588bedc4895a7271cf1a2a848cd46fd5
Vinay Kumar Yerra <vicky@Vinays-MacBook-Pro.local>
2025-05-05 18:38:47 +0530

    Solved some more problems

---
+++ sum_root_to_leaf_numbers.py
@@ -0,0 +1,63 @@
+"""
+URL: https://leetcode.com/problems/sum-root-to-leaf-numbers
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def helper(node, number, sumSoFar):
+            updatedNumber = number * 10 + node.val
+            if node.left is None and node.right is None:
+                return sumSoFar + updatedNumber
+            if node.left:
+                sumSoFar = helper(node.left, updatedNumber, sumSoFar)
+            if node.right:
+                sumSoFar = helper(node.right, updatedNumber, sumSoFar)
+            return sumSoFar
+
+        return helper(root, 0, 0)
---
+++ two_sum.py
@@ -0,0 +1,14 @@
+"""
+URL:
+"""
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+        for i, num in enumerate(nums):
+            if target - num in visited:
+                return [visited[target - num], i]
+            visited[num] = i
+        return []
