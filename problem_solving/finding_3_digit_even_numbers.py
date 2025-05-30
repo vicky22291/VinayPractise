1fe2ca2534225fb84df080edd2ad92b7669b5ca9
Vinay Kumar Yerra <vicky@Vinays-MacBook-Pro.local>
2025-05-31 02:53:49 +0530

    Solved some more problems.

---
+++ problem_solving/finding_3_digit_even_numbers.py
@@ -0,0 +1,56 @@
+"""
+URL: https://leetcode.com/problems/finding-3-digit-even-numbers/description
+
+You are given an integer array digits, where each element is a digit. The array may contain duplicates.
+
+You need to find all the unique integers that follow the given requirements:
+
+The integer consists of the concatenation of three elements from digits in any arbitrary order.
+The integer does not have leading zeros.
+The integer is even.
+For example, if the given digits were [1, 2, 3], integers 132 and 312 follow the requirements.
+
+Return a sorted array of the unique integers.
+
+
+
+Example 1:
+
+Input: digits = [2,1,3,0]
+Output: [102,120,130,132,210,230,302,310,312,320]
+Explanation: All the possible integers that follow the requirements are in the output array.
+Notice that there are no odd integers or integers with leading zeros.
+Example 2:
+
+Input: digits = [2,2,8,8,2]
+Output: [222,228,282,288,822,828,882]
+Explanation: The same digit can be used as many times as it appears in digits.
+In this example, the digit 8 is used twice each time in 288, 828, and 882.
+Example 3:
+
+Input: digits = [3,7,5]
+Output: []
+Explanation: No even integers can be formed using the given digits.
+
+
+Constraints:
+
+3 <= digits.length <= 100
+0 <= digits[i] <= 9
+"""
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        res = []  # Target even number array
+        freq = Counter(
+            digits
+        )  # The frequency of each number in the integer array
+        # Enumerate all three-digit even numbers, maintain the frequency of each digit in the integer, and compare and judge whether it is the target even number
+        for i in range(100, 1000, 2):
+            freq1 = Counter([int(d) for d in str(i)])
+            if all(freq[d] >= freq1[d] for d in freq1.keys()):
+                res.append(i)
+        return res
